8600d99f463a520a0652c68993d2a78bc8802345
halvormundal <halvorbmundal@gmail.com>
2023-06-02 14:05:43 +0200

    Sett opp prettier og eslint og kjør lint

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'd8ycstqz',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
